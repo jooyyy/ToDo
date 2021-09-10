36119fd026f869a3b93e77d33859c5c2393892f3
joy <joy@iMac.local>
2021-09-10 15:23:40 +0800

    save

--- model/task.go
+++ model/task.go
@@ -5,4 +5,12 @@ import "github.com/jinzhu/gorm"
 type Task struct {
 	gorm.Model
 	Name 			string
+	Description 	string `gorm:"type:text"`
+	WorkDurations 	[]WorkDuration `gorm:"type:json"`
+
+}
+
+type WorkDuration struct {
+	StartTimestamp 		int64
+	EndTimestamp 		int64
 }
